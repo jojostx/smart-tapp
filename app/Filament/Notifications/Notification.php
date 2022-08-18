7b00e3e253b90bb07667c4c8f25f0743819e278e
Ari Jay <ikuskid7@gmail.com>
2022-08-18 17:07:44 +0100

    create Filament TimedActionNotification that supports actions that fire an event after a timeout

---
+++ app/Filament/Notifications/Notification.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Notifications;
+
+use Filament\Notifications\Notification as BaseNotification;
+
+class Notification extends BaseNotification
+{
+  public function getActions(): array
+  {
+    return $this->evaluate($this->actions, ['duration' => $this->getDuration()]);
+  }
+}
