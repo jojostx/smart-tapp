0f9c73ab2d01fe10ee316651efe612402d500341
Ari Jay <ikuskid7@gmail.com>
2022-08-18 14:28:36 +0100

    comply with interface method typehints

--- app/Traits/AccessActivationNotifiable.php
+++ app/Traits/AccessActivationNotifiable.php
@@ -2,17 +2,22 @@
 
 namespace App\Traits;
 
+use App\Models\Tenant\User;
 use App\Notifications\Tenant\Driver\AccessActivationNotification;
+use Illuminate\Database\Eloquent\Model;
 
 trait AccessActivationNotifiable
 {
   /**
    * send the access activation notification (SMS) to the access' driver phone number.
-   *
+   * @param \App\Models\Tenant\User $user
+   * 
    * @return void
    */
-  public function sendAccessActivationNotification(): void
+  public function sendAccessActivationNotification(?Model $admin = null): void
   {
-    $this->driver->notify(new AccessActivationNotification($this));
+    $admin = $admin ?? auth()->user();
+
+    $this->driver->notify(new AccessActivationNotification($this, $admin));
   }
 }
