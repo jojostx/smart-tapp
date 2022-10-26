6b93cea02316a319d329e5ded1ad4b7f4c82bf69
Ari Jay <ikuskid7@gmail.com>
2022-10-26 11:45:17 +0100

    refactor: fix logo visibility at different viewpoints

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -1,4 +1,4 @@
 <div class="relative flex overflow-hidden after:inset-y-0 after:right-0 after:w-1/6 after:bg-gradient-to-r after:from-transparent after:to-white after:absolute">
   <x-logo class="flex-shrink-0 w-auto mr-3 text-primary-600 h-9" alt="{{ config('app.name') }} Logo" />
-  <span class="self-center hidden text-xl font-semibold whitespace-nowrap md:inline">{{ tenant('organization') }}</span>
+  <span class="self-center text-xl font-semibold whitespace-nowrap">{{ tenant('organization') }}</span>
 </div>
