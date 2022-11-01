37ddbf995f8885ca996a6b41535c502aba46ffee
Ari Jay <ikuskid7@gmail.com>
2022-11-01 10:29:20 +0100

    feat: add deactivation functionality to User edit page

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -2,8 +2,8 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Enums\Models\UserAccountStatus;
 use App\Filament\Resources\UserResource;
+use App\Filament\Traits\WithCurrentPasswordField;
 use App\Models\Tenant\User;
 use Filament\Pages\Actions;
 use Filament\Pages\Actions\Action;
@@ -12,7 +12,14 @@ use Spatie\Permission\PermissionRegistrar;
 
 class EditUser extends EditRecord
 {
+    use WithCurrentPasswordField;
+    
     protected static string $resource = UserResource::class;
+
+    protected function getListeners()
+    {
+        return ['$refresh'];
+    }
 
     protected function getActions(): array
     {
@@ -22,16 +29,30 @@ class EditUser extends EditRecord
 
         $user = $this->record;
 
-        if ($user instanceof User && $user->isInactive()) {
+        if ($user instanceof User) {
             $actions = array_merge(
                 [
                     Action::make('resend')
                         ->label('Send Activation Email')
+                        ->visible(fn () => $user->isInactive())
                         ->action(function () use ($user) {
                             $user->sendCreateNewPasswordNotification();
-
                             app(PermissionRegistrar::class)->forgetCachedPermissions();
+                            $this->emitSelf('$refresh');
+                        }),
+                    Action::make('toggle_status')
+                        ->label(fn () => $user->isActive() ? 'Deactivate' : 'Activate')
+                        ->button()
+                        ->action(function () use ($user) {
+                            $user->isActive() ? $user->deactivateAccount() : $user->activateAccount();
+                            app(PermissionRegistrar::class)->forgetCachedPermissions();
+                            $this->emitSelf('$refresh');                            
                         })
+                        ->requiresConfirmation()
+                        ->form([
+                            static::getCurrentPasswordField()
+                        ])
+                        ->color(fn () => $user->isActive() ? 'warning' : 'primary'),
                 ],
                 $actions
             );
