a5773c8821c7918e88d915945363e6ee38b19a66
Ari Jay <ikuskid7@gmail.com>
2022-11-03 15:55:05 +0100

    fix: invoke the getIcon closure instead of passing it as a prop

--- resources/views/vendor/filament/forms/components/help-card.blade.php
+++ resources/views/vendor/filament/forms/components/help-card.blade.php
@@ -1,7 +1,7 @@
 <x-filament::card>
     <div {{ $attributes->merge($getExtraAttributes())->class('border rounded-xl my-2 p-4 border-primary-400 grid grid-cols-12 gap-4 filament-forms-placeholder-component') }}>
         <div class="col-span-1 text-primary-700">
-            <x-dynamic-component :component="$getIcon" class="shrink-0 min-w-[26px]" />
+            <x-dynamic-component :component="$getIcon()" class="shrink-0 min-w-[26px]" />
         </div>
         <div class="col-span-11">
           {{ $getContent() }}
