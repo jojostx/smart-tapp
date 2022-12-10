19760378db0708309c73281291649dc9e1a1efcd
Ari Jay <ikuskid7@gmail.com>
2022-12-10 03:35:38 +0100

    WIP: implement customer support chat widget

--- resources/views/livewire/tenant/access/dashboard.blade.php
+++ resources/views/livewire/tenant/access/dashboard.blade.php
@@ -31,7 +31,7 @@
           <div class="flex flex-col p-6">
             <h3 class="text-sm font-semibold text-gray-700">
               Expires
-              <span x-data="{ tooltip: 'Your access to this dashboard will be disabled after this period.' }" x-tooltip="tooltip" x-on:click="$tooltip(tooltip, { timeout: 5000 })">
+              <span x-data="{ tooltip: 'Your access to this dashboard will be disabled after this period.' }" x-tooltip="tooltip" @click="$tooltip(tooltip, { timeout: 5000 })">
                 <x-heroicon-o-information-circle class="inline-flex w-4 h-4 text-gray-500" />
               </span>
             </h3>
@@ -51,7 +51,7 @@
 
         @if (!$this->isBlockingAnother)
         <div class="flex items-center justify-center p-6 border-t border-gray-300 :border-gray-700">
-          <x-filament::button icon="heroicon-o-device-mobile" x-on:click="$dispatch('open-modal', { id: 'request-repark' })">
+          <x-filament::button icon="heroicon-o-device-mobile" @click="$dispatch('open-modal', { id: 'request-repark' })">
             {{ __('Request Repark') }}
           </x-filament::button>
         </div>
@@ -76,7 +76,7 @@
 
   <!-- trap focus on modal when open -->
   <form wire:submit.prevent="submit">
-    <x-filament::modal x-on:click.outside="$dispatch('close-modal', {id: 'request-repark'})" width="md" id="request-repark" heading="Request Repark">
+    <x-filament::modal @click.outside="$dispatch('close-modal', {id: 'request-repark'})" width="md" id="request-repark" heading="Request Repark">
       <x-slot name="subheading">
         Fill in the Plate number of the vehicle that is blocking you and the Driver will be notified to repark their vehicle.
       </x-slot>
@@ -87,7 +87,7 @@
 
       <x-slot name="footer">
         <div class="flex justify-end space-x-2 rtl:space-x-reverse">
-          <x-filament::button size='sm' color="secondary" wire:click="cancelRequest" x-on:click="$dispatch('close-modal', {id: 'request-repark'})">
+          <x-filament::button size='sm' color="secondary" wire:click="cancelRequest" @click="$dispatch('close-modal', {id: 'request-repark'})">
             {{ __('Cancel') }}
           </x-filament::button>
 
@@ -99,54 +99,155 @@
     </x-filament::modal>
   </form>
 
-  <!-- botman contact support -->
-  <div id="botmanWidgetRoot" x-data>
-    <div style="position: fixed; bottom: 0px; right: 0px; z-index: 2147483647; box-sizing: content-box; overflow: hidden; min-width: 400px; min-height: 120px;">
-      <div style="position: relative; cursor: pointer;">
-        <button x-tooltip.raw="Contact Support" class="absolute flex justify-center w-[60px] h-[60px] p-2 rounded-full top-10 right-5 bg-primary-800 desktop-closed-message-avatar shadow-md">
-          <x-heroicon-o-chat class="w-full h-auto text-white rounded-full" />
+  <!-- contact support -->
+  <div x-data="{
+      open: true,
+      sendMessage: function() {
+          const chatbox = $refs.chatbox;
+          const chatboxInput = $refs.chatbox_input;
+
+          if ((message = chatboxInput.value?.trim()) == '') {
+            return;
+          }
+          
+          const currentdate = new Date();
+          let options = {hour: '2-digit', minute: '2-digit'};
+          let time = currentdate.toLocaleTimeString('en-us', options)
+
+          const bubble = `
+            <div class='flex flex-row-reverse mb-4'>
+              <div class='flex flex-col items-center flex-none ml-4 space-y-1'>
+                <img class='w-6 h-6 rounded-full' src='https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' />
+                <span class='block text-xs'>You</span>
+              </div>
+              <div class='relative flex-1 mb-2'>
+                <div class='p-2 text-sm text-gray-800 rounded-lg bg-primary-100'>${message}</div>
+                <span class='text-xs leading-none text-gray-600'>${time}</span>
+              </div>
+            </div>`;
+
+          chatboxInput.value = '';
+          chatbox.insertAdjacentHTML('beforeend', bubble);
+          chatbox.scrollTop = chatbox.scrollHeight;
+      }
+    }" 
+    @keyup.escape.window="open = false">
+    <div class="fixed bottom-0 right-0 z-50 overflow-hidden md:mr-4 md:mb-4" style="min-width: 400px;">
+      <div class="relative flex flex-col items-end mb-4 mr-4 space-y-4">
+        <div id="chat_modal" x-show="open" @click.outside="open = false" x-trap="open" x-cloak class="flex flex-col bg-white border shadow-xl dark:bg-gray-800 sm:rounded-lg w-80 h-96">
+          <nav class="p-2 space-y-2 border-b divide-y shadow">
+            <div class="flex items-center justify-between">
+              <!-- user info -->
+              <div class="flex items-center">
+                <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+                <div class="pl-2">
+                  <p class="font-semibold">John Doe</p>
+                  <p class="text-xs text-gray-600">Support Agent</p>
+                </div>
+              </div>
+              <!-- end user info -->
+  
+              <!-- chat box action -->
+              <div>
+                <a class="inline-flex p-2 rounded-full hover:bg-primary-50" href="tel:+23437198917">
+                  <x-heroicon-o-phone class="w-6 h-6" />
+                </a>
+                
+                <button @click="open = false" class="inline-flex p-2 rounded-full hover:bg-primary-50" type="button">
+                  <x-heroicon-o-x class="w-6 h-6" />
+                </button>
+              </div>
+              <!-- end chat box action -->
+            </div>
+            <div class="pt-2">
+              <span class="flex text-xs text-gray-600">
+                <span>
+                  <svg class="w-4 h-4" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path clip-rule="evenodd" d="M7 13A6 6 0 107 1a6 6 0 000 12z" stroke="#A2A2A2" stroke-linecap="round" stroke-linejoin="round"/><path d="M7 9.999v-3.5a.5.5 0 00-.5-.5h-1m.75-2.5a.25.25 0 110 .5.25.25 0 010-.5M5.5 10h3" stroke="#A2A2A2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                </span>
+                <span>
+                  &nbsp;Typically responds within 30 mins
+                </span>
+              </span>
+            </div>
+          </nav>
+
+          <!-- <div x-ref="chatbox" class="flex-1 px-4 py-4 space-y-4 overflow-y-auto"> -->
+            <!-- chat message -->
+            <!-- <div class="flex">
+              <div class="flex flex-col items-center flex-none mr-4 space-y-1">
+                <img class="w-6 h-6 rounded-full" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+              </div>
+              <div class="relative flex-1">
+                <div class="p-2 text-sm text-white rounded-lg bg-primary-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                <span class="text-xs leading-none text-primary-600">2 min ago</span>
+              </div>
+            </div> -->
+            <!-- end chat message -->
+
+            <!-- chat message -->
+            <!-- <div class="flex flex-row-reverse">
+              <div class="flex flex-col items-center flex-none ml-4 space-y-1">
+                <img class="w-6 h-6 rounded-full" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+                <span class='block text-xs'>You</span>
+              </div>
+              <div class="relative flex-1">
+                <div class="p-2 text-sm text-gray-800 rounded-lg bg-primary-100">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                <span class="text-xs leading-none text-gray-600">2 min ago</span>
+              </div>
+            </div> -->
+            <!-- end chat message -->
+            
+            <!-- chat message -->
+            <!-- <div class="flex">
+              <div class="flex flex-col items-center flex-none mr-4 space-y-1">
+                <img class="w-6 h-6 rounded-full" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+              </div>
+              <div class="relative flex-1">
+                <div class="p-2 text-sm text-white rounded-lg bg-primary-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                <span class="text-xs leading-none text-primary-600">2 min ago</span>
+              </div>
+            </div> -->
+            <!-- end chat message -->
+            
+            <!-- chat message -->
+            <!-- <div class="flex flex-row-reverse">
+              <div class="flex flex-col items-center flex-none ml-4 space-y-1">
+                <img class="w-6 h-6 rounded-full" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+                <span class='block text-xs'>You</span>
+              </div>
+              <div class="relative flex-1">
+                <div class="p-2 text-sm text-gray-800 rounded-lg bg-primary-100">consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                <span class="text-xs leading-none text-gray-600">2 min ago</span>
+              </div>
+            </div> -->
+            <!-- end chat message -->
+          <!-- </div> -->
+
+          <!-- <div class="flex items-center p-3 space-x-2 border-t"> -->
+            <!-- <div class="w-full">
+              <input x-ref="chatbox_input" @keyup.enter="sendMessage" class="w-full border border-gray-300 rounded-md" type="text" value="" placeholder="Aa" autofocus />
+            </div> -->
+
+            <!-- chat send action -->
+            <!-- <div>
+              <button @click="sendMessage" class="inline-flex p-2 rounded-full hover:bg-primary-50" type="button">
+                <x-heroicon-o-paper-airplane class="w-6 h-6 rotate-90 rounded-full" />
+              </button>
+            </div> -->
+            <!-- end chat send action -->
+          <!-- </div> -->
+        </div>
+
+        <!-- docked bottom right corner -->
+        <button x-tooltip.raw="Contact Support" @click="open = !open" class="relative flex justify-center w-12 h-12 p-2 transform rounded-full shadow-md bg-primary-800">
+          <span id="notification-ping" class="absolute top-0 right-0 flex w-3 h-3">
+            <span class="absolute inline-flex w-full h-full rounded-full opacity-75 animate-ping bg-danger-400"></span>
+            <span class="relative inline-flex w-3 h-3 rounded-full bg-danger-500"></span>
+          </span>
+          <x-heroicon-o-chat class="w-full h-auto text-white" />
         </button>
-      </div>
-      <div style="display: none; height: 209px;">
+        <!-- end docked bottom right corner -->
       </div>
     </div>
   </div>
 </div>
-<!-- 
-<th scope="col" x-data="{ open: false }" x-on:keydown.escape.stop="open = false" x-on:mousedown.away="open = false" wire:key="header-col-8-HQuhCDoT3e8BGFKEQWoC" class="relative inline-flex items-center px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 whitespace-nowrap dark:bg-gray-800 dark:text-gray-400">
-  <span class="uppercase">
-    Tags
-  </span>
-
-  <button type="button" class="inline-flex justify-center ml-2 text-gray-500" x-on:click="open = !open" aria-haspopup="true" x-bind:aria-expanded="open" aria-expanded="false">
-    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-      <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-    </svg>
-  </button>
-
-  <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" role="menu" aria-orientation="vertical" aria-labelledby="filters-menu" class="absolute left-0 z-50 w-full mt-2 bg-white divide-y divide-gray-100 rounded-md shadow-lg top-6 md:w-56 ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-700 dark:text-white dark:divide-gray-600" style="top: 30px; display: none;">
-    <div class="py-1" role="none">
-      <div class="block px-4 py-2 space-y-1 text-sm text-gray-700" role="menuitem">
-        <label for="users1-filter-email_verified_at" class="block text-sm font-medium text-gray-700 ">
-          Search Tags
-        </label>
-
-        <div class="flex rounded-md shadow-sm">
-          <input wire:model="users1.search" type="text" class="block w-full transition duration-150 ease-in-out border-gray-300 rounded-md shadow-sm sm:text-sm sm:leading-5 dark:bg-gray-700 dark:text-white dark:border-gray-600 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Start typing">
-        </div>
-      </div>
-    </div>
-  </div>
-</th>
-
-<th scope="col" wire:key="header-col-8-HQuhCDoT3e8BGFKEQWoC" class="relative inline-flex items-center px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 whitespace-nowrap dark:bg-gray-800 dark:text-gray-400">
-  <span class="uppercase">
-    Tags
-  </span>
-
-  <button type="button" class="inline-flex justify-center ml-2 text-gray-500" aria-haspopup="true" aria-expanded="false">
-    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-      <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-    </svg>
-  </button>
-</th> -->
