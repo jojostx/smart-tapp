06c3858c4b9e7d15707768537306b8ac2dbf518a
Ari Jay <ikuskid7@gmail.com>
2022-12-14 17:23:23 +0100

    add required validation rules and hooks

--- app/Filament/Resources/AccessResource/Pages/CreateAccess.php
+++ app/Filament/Resources/AccessResource/Pages/CreateAccess.php
@@ -8,15 +8,16 @@ use App\Models\Tenant\Access;
 use App\Models\Tenant\Driver;
 use App\Models\Tenant\ParkingLot;
 use App\Models\Tenant\Vehicle;
-use Filament\Pages\Actions;
 use Filament\Pages\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class CreateAccess extends CreateRecord
 {
     protected static string $resource = AccessResource::class;
+    public $vehicle;
+    public $driver;
+    public $parking_lot;
 
     protected function getCancelFormAction(): Action
     {
@@ -29,61 +30,66 @@ class CreateAccess extends CreateRecord
             ->color('secondary');
     }
 
-    protected function handleRecordCreation(array $data): Model
+    public function beforeCreate()
     {
-        // find vehicle
-        $vehicle = Vehicle::find(intval($data['vehicle_id']));
+        $data = $this->form->getState();
 
-        if (blank($vehicle)) {
+        // find vehicle
+        $this->vehicle = Vehicle::find(intval($data['vehicle_id']));
+
+
+        if (blank($this->vehicle)) {
             return $this->addError('vehicle_id', 'Unable to create Access for invalid Vehicle');
         }
 
         // find driver
-        $driver = Driver::find(intval($data['driver_id']));
+        $this->driver = Driver::find(intval($data['driver_id']));
 
-        if (blank($driver)) {
+        if (blank($this->driver)) {
             return $this->addError('driver_id', 'Unable to create Access for invalid Driver');
         }
 
         // find Parking Lot
-        $parking_lot = ParkingLot::find(intval($data['parking_lot_id']));
+        $this->parking_lot = ParkingLot::find(intval($data['parking_lot_id']));
 
-        if (blank($parking_lot)) {
+        if (blank($this->parking_lot)) {
             return $this->addError('parking_lot_id', 'Unable to create Access for invalid Parking Lot');
         }
+    }
 
-        /**
-         * @var Access $access
-         */
-        $access = new (static::getModel());
+    protected function handleRecordCreation(array $data): Model
+    {
+        $access = new Access;
 
         // create access, attach driver and vehicle models and set it's status
-        try { 
-            DB::beginTransaction(); // Tell Laravel all the code beneath this is a transaction
-
-            $access->driver()->associate($driver);
-            $access->vehicle()->associate($vehicle);
-            $access->parkingLot()->associate($parking_lot);
+        try {
+            $access->driver()->associate($this->driver);
+            $access->vehicle()->associate($this->vehicle);
+            $access->parkingLot()->associate($this->parking_lot);
             $access->creator()->associate(auth()->user());
             $access->issuer()->associate(auth()->user());
-            
+
             $access->fill([
                 "expiry_period" => intval($data['expiry_period']),
                 "validity_period" => intval($data['validity_period']),
             ]);
 
-            $return_value = match (AccessStatus::from($data['status'])) {
+            $saved = match ($status = AccessStatus::from($data['status'])) {
                 AccessStatus::ISSUED => $access->issue(),
                 AccessStatus::ACTIVE => $access->activate(),
                 AccessStatus::INACTIVE => $access->deactivate(),
                 default => false,
             };
 
-            DB::commit();
+            if ($saved && \in_array($status, [AccessStatus::ACTIVE, AccessStatus::ISSUED])) {
+                $access->sendAccessActivationNotification(checkStatusCountdown: 30);
+            }
 
-            return $return_value && $access->refresh();
+            \dd($access);
+
+            return $access->refresh();
         } catch (\Throwable $th) {
-            DB::rollBack(); // Tell Laravel, "It's not you, it's me. Please don't persist to DB"
+            \dd($access);
 
             return $access;
         }
