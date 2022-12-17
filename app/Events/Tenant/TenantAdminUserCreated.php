fcddad68ef6acc42db605187cf75aaac95008aa4
Ari Jay <ikuskid7@gmail.com>
2022-12-17 22:42:42 +0100

    add relevant constructor args to event

--- app/Events/Tenant/TenantAdminUserCreated.php
+++ app/Events/Tenant/TenantAdminUserCreated.php
@@ -2,25 +2,30 @@
 
 namespace App\Events\Tenant;
 
+use App\Models\Tenant;
+use App\Models\Tenant\User;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Stancl\Tenancy\Events\Contracts\TenantEvent;
 
-class TenantAdminUserCreated
+class TenantAdminUserCreated extends TenantEvent
 {
     use Dispatchable;
     use InteractsWithSockets;
     use SerializesModels;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    /** @var Tenant */
+    public $tenant;
+
+    /** @var User */
+    public $user;
+
+    public function __construct(Tenant $tenant, User $user)
     {
-        //
+        $this->tenant = $tenant;
+        $this->user = $user;
     }
 
     /**
