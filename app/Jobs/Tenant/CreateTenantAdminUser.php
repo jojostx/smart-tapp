a743031af090d76029a7922e2d16e27c7d09bd4c
Ari Jay <ikuskid7@gmail.com>
2022-12-17 22:39:33 +0100

    refactor to use action

--- app/Jobs/Tenant/CreateTenantAdminUser.php
+++ app/Jobs/Tenant/CreateTenantAdminUser.php
@@ -2,9 +2,8 @@
 
 namespace App\Jobs\Tenant;
 
-use App\Enums\Models\UserAccountStatus;
+use App\Actions\CreateTenantAdminUserAction;
 use App\Models\Tenant;
-use App\Models\Tenant\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -37,20 +36,6 @@ class CreateTenantAdminUser implements ShouldQueue
      */
     public function handle()
     {
-        /** @param  \App\Models\Tenant  $tenant */
-        $this->tenant->run(function ($tenant) {
-            // create the user model in the tenant's db and set the tenant_id
-            $user = User::create([
-                'tenant_id' => $tenant->id,
-                'name' => $tenant->name,
-                'email' => $tenant->email,
-                'password' => $tenant->password,
-            ]);
-
-            $user->forceFill([
-                'email_verified_at' => $tenant->email_verified_at,
-                'status' => UserAccountStatus::ACTIVE,
-            ])->save();
-        });
+        (new CreateTenantAdminUserAction)($this->tenant);
     }
 }
