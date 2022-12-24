3dcdf84899d686975d1a4f12accc395a7744edef
Ari Jay <ikuskid7@gmail.com>
2022-12-24 08:53:06 -0500

    remove db transaction that automatically rollback without throwing error or creating user

--- app/Actions/CreateTenantAdminUserAction.php
+++ app/Actions/CreateTenantAdminUserAction.php
@@ -7,7 +7,6 @@ use App\Enums\Roles\UserRole;
 use App\Events\Tenant\TenantAdminUserCreated;
 use App\Models\Tenant;
 use App\Models\Tenant\User;
-use Illuminate\Support\Facades\DB;
 
 class CreateTenantAdminUserAction
 {
@@ -16,36 +15,34 @@ class CreateTenantAdminUserAction
     // change plan for current subscription and allow sync
     public function __invoke(Tenant $tenant): ?User
     {
-        $user = $tenant->run(function ($tenant) {
-            // create the user model in the tenant's db and set the tenant_id
-            try {
-                DB::beginTransaction();
-
-                $user = User::create([
+        try {
+            $user = $tenant->run(function ($tenant) {
+                // create the user model in the tenant's db and set the tenant_id
+                $user = User::firstOrCreate([
                     'tenant_id' => $tenant->id,
-                    'name' => $tenant->name,
                     'email' => $tenant->email,
+                ],
+                [
+                    'name' => $tenant->name,
                     'password' => $tenant->password,
                 ]);
 
-                $user->forceFill([
+                $saved = $user->forceFill([
                     'email_verified_at' => $tenant->email_verified_at,
                     'status' => UserAccountStatus::ACTIVE,
                 ])->save();
 
-                $user->assignRole(UserRole::SUPER_ADMIN->value);
-
-                DB::commit();
-
-                \event(new TenantAdminUserCreated($tenant, $user));
+                if (\filled($user) && $saved && !$user->hasRole(UserRole::SUPER_ADMIN->value)) {
+                    $user->assignRole(UserRole::SUPER_ADMIN->value);
+                }
 
                 return $user;
-            } catch (\Exception $exp) {
-                DB::rollBack();
+            });
 
-                return null;
-            }
-        });
+            \event(new TenantAdminUserCreated($tenant, $user));
+        } catch (\Throwable $exp) {
+            return null;
+        }
 
         return $user;
     }
