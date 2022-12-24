b2ce0c28150ffa8141468f31c0a264d500b0045d
Ari Jay <ikuskid7@gmail.com>
2022-12-24 08:49:25 -0500

    add job to clear tenant subdomain

---
+++ app/Jobs/Tenant/DeleteTenantSubdomain.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs\Tenant;
+
+use App\Models\Tenant;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteTenantSubdomain implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    protected $tenant;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Tenant $tenant)
+    {
+        $this->tenant = $tenant;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        if ($this->tenant->domains?->isEmpty()) {
+            return;
+        }
+
+        return $this->tenant->domains->each->delete();
+    }
+}
